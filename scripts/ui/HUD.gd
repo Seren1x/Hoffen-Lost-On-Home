458e44c7a86e66ce76c726d716ce763b590642e3
rexy <rexy.adrian.f@gmail.com>
2026-08-24 22:13:44 +0700

    ada progress

--- scripts/ui/HUD.gd
+++ scripts/ui/HUD.gd
@@ -1,41 +1,15 @@
 extends CanvasLayer
 class_name HUD
 
-const HEART_FULL := preload("res://assets/ui/UI - PostApocalypse/HP/Heart_Full.png")
-const HEART_HALF := preload("res://assets/ui/UI - PostApocalypse/HP/Heart_Half.png")
-const HEART_EMPTY := preload("res://assets/ui/UI - PostApocalypse/HP/Heart_Empty.png")
-const BULLET_FULL := preload("res://assets/ui/UI - PostApocalypse/Bullet Indicators/Pistol-Bullet.png")
-const BULLET_EMPTY := preload("res://assets/ui/UI - PostApocalypse/Bullet Indicators/Pistol-Bullet_Empty.png")
+@onready var hp_bar: TextureProgressBar = %HPBar
+@onready var energy_bar: TextureProgressBar = %EnergyBar
 
-@onready var hearts_container: HBoxContainer = %Hearts
-@onready var stamina_bar: ProgressBar = %StaminaBar
-@onready var bullets_container: HBoxContainer = %Bullets
 
 func update_health(current: int, maximum: int) -> void:
-	var heart_count: int = hearts_container.get_child_count()
-	var hp_per_heart: int = maxi(1, int(float(maximum) / float(heart_count)))
+	hp_bar.max_value = float(maximum)
+	hp_bar.value = float(current)
 
-	for i: int in heart_count:
-		var heart: TextureRect = hearts_container.get_child(i) as TextureRect
-		if not heart:
-			continue
-		var threshold: int = (i + 1) * hp_per_heart
-		var prev_threshold: int = i * hp_per_heart
 
-		if current >= threshold:
-			heart.texture = HEART_FULL
-		elif current > prev_threshold:
-			heart.texture = HEART_HALF
-		else:
-			heart.texture = HEART_EMPTY
-
-func update_stamina(current: float, maximum: float) -> void:
-	stamina_bar.max_value = maximum
-	stamina_bar.value = current
-
-func update_ammo(current: int, _maximum: int) -> void:
-	for i: int in bullets_container.get_child_count():
-		var bullet: TextureRect = bullets_container.get_child(i) as TextureRect
-		if not bullet:
-			continue
-		bullet.texture = BULLET_FULL if i < current else BULLET_EMPTY
+func update_energy(current: float, maximum: float) -> void:
+	energy_bar.max_value = maximum
+	energy_bar.value = current
--- scripts/ui/PauseMenu.gd
+++ scripts/ui/PauseMenu.gd
@@ -3,12 +3,21 @@ class_name PauseMenu
 
 @export var settings_scene: PackedScene
 
+var _hud: HUD
+
+@onready var _stats_panel: VBoxContainer = %StatsPanel
+@onready var _hp_label: Label = %HPLabel
+@onready var _energy_label: Label = %EnergyLabel
+
+
 func _ready() -> void:
 	process_mode = PROCESS_MODE_ALWAYS
 	hide()
+	_hud = _find_hud()
 	%ResumeButton.pressed.connect(_on_resume_pressed)
 	%SettingsButton.pressed.connect(_on_settings_pressed)
 	%ExitToMainButton.pressed.connect(_on_quit_to_menu_pressed)
+
 
 func _input(event: InputEvent) -> void:
 	if not (event is InputEventKey):
@@ -19,16 +28,40 @@ func _input(event: InputEvent) -> void:
 		else:
 			_pause()
 
+
+func _find_hud() -> HUD:
+	var parent: Node = get_parent()
+	if parent:
+		return parent.get_node_or_null("HUD") as HUD
+	return null
+
+
 func _pause() -> void:
 	get_tree().paused = true
+	if _hud:
+		_hud.hide()
+		_refresh_stats()
 	show()
+
 
 func _resume() -> void:
 	get_tree().paused = false
+	if _hud:
+		_hud.show()
 	hide()
+
+
+func _refresh_stats() -> void:
+	if not _hud:
+		return
+	_hp_label.text = "HP: %d / %d" % [int(_hud.hp_bar.value), int(_hud.hp_bar.max_value)]
+	_energy_label.text = "Energy: %d / %d" % [int(_hud.energy_bar.value), int(_hud.energy_bar.max_value)]
+	_stats_panel.show()
+
 
 func _on_resume_pressed() -> void:
 	_resume()
+
 
 func _on_settings_pressed() -> void:
 	if not settings_scene:
@@ -37,8 +70,10 @@ func _on_settings_pressed() -> void:
 	settings.back_pressed.connect(_on_settings_closed.bind(settings))
 	add_child(settings)
 
+
 func _on_settings_closed(settings: Control) -> void:
 	settings.queue_free()
+
 
 func _on_quit_to_menu_pressed() -> void:
 	get_tree().paused = false
