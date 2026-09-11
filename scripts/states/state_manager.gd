d9e2ebd5abcac112c27fec630afc298bba388b6f
Rainmo0 <aunknown145@gmail.com>
2026-09-11 23:44:50 +0700

    badag

--- scripts/states/state_manager.gd
+++ scripts/states/state_manager.gd
@@ -31,7 +31,7 @@ var saved_weapon_data: Dictionary = {}
 func _ready() -> void:
 	# First state the game boots into. Change this if you want to skip
 	# straight to "playing" while testing.
-	change_state("main_menu")
+	change_state("end_screen")
 
 
 func current_state() -> Node:   # was: -> GameState
